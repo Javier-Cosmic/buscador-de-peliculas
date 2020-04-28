c309a27eb3ee83f6e8892e3733c2ad993167ab00
javier_contreras <javiercontreras.tome@gmail.com>
2020-04-28 19:32:25 -0400

    Last version

--- src/components/Titulo.js
+++ src/components/Titulo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
-import logo from '../assets/logo.png'
+import logo from '../assets/img/logo.png'
 
 export const Titulo = ({ children }) => {
 
