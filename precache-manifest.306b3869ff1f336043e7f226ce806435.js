186752066538fd7a15e577758b5d50995c0ed23c
javier_contreras <javiercontreras.tome@gmail.com>
2020-04-28 22:22:42 -0400

    Updates

---
+++ precache-manifest.306b3869ff1f336043e7f226ce806435.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8d0cd34d25362743105395d5561c67",
+    "url": "/buscador-de-peliculas/index.html"
+  },
+  {
+    "revision": "b4c0b61be98af3004bdf",
+    "url": "/buscador-de-peliculas/static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "06b84d1d8e0dde295bca",
+    "url": "/buscador-de-peliculas/static/css/main.ab057522.chunk.css"
+  },
+  {
+    "revision": "b4c0b61be98af3004bdf",
+    "url": "/buscador-de-peliculas/static/js/2.fb49768a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/buscador-de-peliculas/static/js/2.fb49768a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06b84d1d8e0dde295bca",
+    "url": "/buscador-de-peliculas/static/js/main.7e485b48.chunk.js"
+  },
+  {
+    "revision": "ee87ad5197b0df1be8cb",
+    "url": "/buscador-de-peliculas/static/js/runtime-main.e1356fba.js"
+  },
+  {
+    "revision": "914202736c35ea8947ff7fa0cac79090",
+    "url": "/buscador-de-peliculas/static/media/logo.91420273.png"
+  }
+]);
